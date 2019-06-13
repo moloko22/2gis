4bfc473f7feab996e1e719ea96b9e8c043490507
Ilya Tsapu <sonofzeus2205@gmail.com>
2019-06-13 21:57:09 +0300

    add pages
    and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AboutAuthorComponent } from './components/about-author/about-author.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AboutAuthorComponent,
+    AuthorizationComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: MainPageComponent
+      },
+      {
+        path:'about',
+        component: AboutAuthorComponent
+      },
+      {
+        path:'auth',
+        component: AuthorizationComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/about-author/about-author.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-author',
+  templateUrl: './about-author.component.html',
+  styleUrls: ['./about-author.component.css']
+})
+export class AboutAuthorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
